275f13bb52139c1e959e5113e3958b427c8e63ee
Liesen Gai <144450938+daomuyang@users.noreply.github.com>
2026-05-22 16:28:38 +0800

    Add files via upload

---
+++ PJ2/vgg_loss_landscape/models/nn.py
@@ -0,0 +1,16 @@
+# nn.py
+import torch
+from torch import nn
+
+def init_weights_(m):
+    """初始化卷积层和全连接层权重"""
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
